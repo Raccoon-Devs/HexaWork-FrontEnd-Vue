55c26f40bdf7d1f0eb1f2617aab5b85b735edca7
José Ramírez <jota.rf.88@gmail.com>
2021-06-29 20:45:58 -0700

    Eliminación de código comentado

--- src/core/infraestructura/adaptadorMockOferta.ts
+++ src/core/infraestructura/adaptadorMockOferta.ts
@@ -1,9 +1,6 @@
 import { APIPuerto } from "../aplicacion/api/APIPuerto";
 import { OfertaParaDominio } from "../dominio/OfertasDeTrabajo/DTOOfertaDeTrabajo";
 import { obtenerOfertas, crearOferta } from "./persistencia/ofertas";
-//import a from "./persistencia/ofertas.json";
-//import * as data from "../persistencia/ofertas"
-
 
 export class AdaptadorMockOferta extends APIPuerto{
     
@@ -14,53 +11,5 @@ export class AdaptadorMockOferta extends APIPuerto{
     public crearOferta(ofertaDeTrabajo: OfertaParaDominio){
 
         crearOferta(ofertaDeTrabajo)
-        // oferta.push(
-        //     {
-        //         id: "7",
-        //         titulo: "Economista",
-        //         direccion: {
-        //             calle1: "calle1",
-        //             calle2: "calle2",
-        //             ciudad: "ciudad",
-        //             estado: "estado",
-        //             zip: "zip"
-        //         },
-        //         fechaLimite: "2020/05/05",
-        //         duracion: 5,
-        //         remuneracion: {
-        //             monto: 400,
-        //             divisa: "dolar",
-        //             frecuencia: "hora"
-        //         },
-        //         estadoOfertaDeTrabajo:  0,
-        //         vacante: 5,
-        //         cargo: "Cargo",
-        //         descripcion: "Descripcion de la oferta",
-        //         fechaPublicacion: "2020/05/05",
-        //         empleador: {
-        //             IDEmpleador: {IDEmpleador:"5"},
-        //             nombreEmpresa: {
-        //                 nombre: "nombreEmpresa",
-        //                 rif: "rif"
-        //             },
-        //             direccion: {
-        //                 calle1: "calle1",
-        //                 calle2: "calle2",
-        //                 ciudad: "ciudad",
-        //                 estado: "estado",
-        //                 zip: "zip"
-        //             },
-        //             infoEmpleador: {
-        //                 nombreCompleto: "string",
-        //                 cargoDeTrabajo: "string",
-        //                 numeroDeTelefono: "string",
-        //                 correo: "string"
-        //             },
-        //             rol: {
-        //                 nombre: "a"
-        //             }
-        //         }
-        //     }
-        // )
     }
 }
