26f90dfff7395b80ab26bf2e537817f52ceb0951
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-07-19 00:42:39 -0400

    WIP: Excepciones de Empleado VO

--- src/core/dominio/Empleado/valueObjects/EstadoEmpleado.ts
+++ src/core/dominio/Empleado/valueObjects/EstadoEmpleado.ts
@@ -1,4 +1,5 @@
 import { ValueObject } from "../../ClasesBase/ValueObject"
+import { ExcepcionEstadoEmpleadoInvalido } from '../excepciones/ExcepcionEstadoEmpleadoInvalido'
 
 interface EstadoEmpleadoPropiedades {
     estado: number
@@ -15,6 +16,12 @@ export class EstadoEmpleado extends ValueObject<EstadoEmpleadoPropiedades> {
     }
 
     public static crear(estado: number): EstadoEmpleado {
+        if (estado < 0 || estado > 5) {
+            throw new ExcepcionEstadoEmpleadoInvalido<typeof estado>(
+                `El valor estado: ${estado} es invalido, debe estar entre 0 y 5`,
+                estado
+            )
+        }
         return new EstadoEmpleado({estado})
     }
 }
--- src/core/dominio/Empleado/valueObjects/NivelEducativo.ts
+++ src/core/dominio/Empleado/valueObjects/NivelEducativo.ts
@@ -1,4 +1,5 @@
 import { ValueObject } from "../../ClasesBase/ValueObject"
+import { ExcepcionNivelEducativoInvalido } from '../excepciones/ExcepcionnivelEducativoInvalido'
 
 interface NivelEducativoPropiedades {
     nivelEducativo: number
@@ -15,6 +16,12 @@ export class NivelEducativo extends ValueObject<NivelEducativoPropiedades> {
     }
 
     public static crear(nivelEducativo: number): NivelEducativo {
+        if (nivelEducativo < 0 || nivelEducativo > 10) {
+            throw new ExcepcionNivelEducativoInvalido<typeof nivelEducativo>(
+                `El valor nivelEducativo: ${nivelEducativo} es invalido, debe estar entre 0 y 10`,
+                nivelEducativo
+            )
+        }
         return new NivelEducativo({nivelEducativo})
     }
 }
