7fe1e015991356c297a47bd2dd45dd7686ebb2c1
José Ramírez <jota.rf.88@gmail.com>
2021-07-23 17:26:36 -0700

    feat: Modificación del formulario de crear oferta
    - Modificación del formulario para crear una oferta desde la inferfaz gráfica para cumplir con los requerimientos.

--- src/infraestructura/components/FormularioOferta.vue
+++ src/infraestructura/components/FormularioOferta.vue
@@ -57,20 +57,90 @@
                     </v-col>
             </v-row>
             <v-card-subtitle class="indigo--text subtitle-1 mt-2 pb-1">
-                Datos principales
+                Habilidades y Certificaciones Requeridas
+            </v-card-subtitle>
+          <v-row class="mt-0 pt-0 ml-0">
+            <v-col cols="6">
+              <v-select
+                  prepend-icon="mdi-check-bold"
+                  v-model="habilidadesSeleccionadas"
+                  :items="habilidades"
+                  item-text="nombre"
+                  item-value="valor"
+                  :menu-props="{ maxHeight: '400' }"
+                  label="Habilidades"
+                  multiple
+              ></v-select>
+            </v-col>
+            <v-col cols="6">
+              <v-select
+                  prepend-icon="mdi-certificate"
+                  v-model="certificacionesSeleccionadas"
+                  :items="certificaciones"
+                  item-text="nombre"
+                  item-value="valor"
+                  :menu-props="{ maxHeight: '400' }"
+                  label="Certificaciones"
+                  multiple
+              ></v-select>
+            </v-col>
+            
+          </v-row>
+            <v-card-subtitle class="indigo--text subtitle-1 mt-2 pb-1">
+                Duración y días de trabajo
             </v-card-subtitle>
             <v-row class="mt-2 ml-0">
-                <!-- <v-col cols="6">
-                    <v-text-field
-                        v-model="ofertaDeTrabajo.cargo"
-                        id= "cargo"
-                        type="cargo"
-                        label="Cargo" placeholder="Cargo de la oferta de trabajo"
-                        class="mt-0 pt-0" prepend-icon="mdi-briefcase"
+              <v-col cols="6">
+                  <v-text-field
+                  id="duracion"
+                      type="duracion"
+                      v-model="ofertaDeTrabajo.duracion"
+                      label="Cantidad De Horas Requeridas" placeholder="5"
+                      class="mt-0 pt-0" prepend-icon="mdi-clock-time-five"
+                  >
+                  </v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-btn class="indigo darken-4" dark @click="dialogCalendario = true">Modificar Calendario</v-btn>
+              </v-col>
+            </v-row>
+            <v-card-subtitle class="indigo--text subtitle-1 mt-2 pb-1">
+                Fecha límite de postulación y cantidad de vacantes
+            </v-card-subtitle>
+            <v-row class="mt-2 ml-0">
+              <v-col
+                  cols="6"
+                >
+                    <v-menu
+                      id="menuFechaLimite"
+                      ref="menuFechaLimite"
+                      v-model="menuFechaLimite"
+                      :close-on-content-click="false"
+                      transition="scale-transition"
+                      offset-y
+                      min-width="auto"
                     >
-                    </v-text-field>
-                </v-col> -->
-                <v-col cols="6">
+                      <template v-slot:activator="{ on, attrs }">
+                        <v-text-field
+                          id="fechaPostulacion"
+                          v-model="ofertaDeTrabajo.fechaLimite"
+                          label="Fecha Límite de Postulación"
+                          prepend-icon="mdi-calendar"
+                          type="fechaPostulacion"
+                          v-bind="attrs"
+                          v-on="on"
+                        ></v-text-field>
+                      </template>
+                      <v-date-picker
+                        v-model="ofertaDeTrabajo.fechaLimite"
+                        :active-picker.sync="fechaLimitePicker"
+                        :min="new Date().toISOString().substr(0, 10)"
+                        @change="guardarFechaLimite"
+                        locale="es-VE"
+                      ></v-date-picker>
+                    </v-menu>
+                </v-col>
+                <v-col cols="6" class="pt-7">
                     <v-text-field
                         id="vacantes"
                         type="vacantes"
@@ -80,66 +150,11 @@
                     >
                     </v-text-field>
                 </v-col>
-          </v-row>
-          <v-row class="mt-0 pt-0 ml-0">
-            <v-col cols="6">
-              <v-menu
-                id="menuFechaLimite"
-                ref="menuFechaLimite"
-                v-model="menuFechaLimite"
-                :close-on-content-click="false"
-                transition="scale-transition"
-                offset-y
-                min-width="auto"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
-                    id="fechaPostulacion"
-                    v-model="ofertaDeTrabajo.fechaLimite"
-                    label="Fecha Límite de Postulación"
-                    prepend-icon="mdi-calendar"
-                    type="fechaPostulacion"
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="ofertaDeTrabajo.fechaLimite"
-                  :active-picker.sync="fechaLimitePicker"
-                  :min="new Date().toISOString().substr(0, 10)"
-                  @change="guardarFechaLimite"
-                  locale="es-VE"
-                ></v-date-picker>
-              </v-menu>
-            </v-col>
-            <v-col cols="6 mt-4">
-                <v-text-field
-                id="duracion"
-                    type="duracion"
-                    v-model="ofertaDeTrabajo.duracion"
-                    label="Cantidad De Horas Requeridas" placeholder="5"
-                    class="mt-0 pt-0" prepend-icon="mdi-clock-time-five"
-                >
-                </v-text-field>
-            </v-col>
-          </v-row>
+            </v-row>
             <v-card-subtitle class="indigo--text subtitle-1 mt-2 pb-1">
                 Remuneración
             </v-card-subtitle>
            <v-row class="mt-2 ml-0">
-            <!-- <v-col cols="6" class="mt-0 pt-0">
-              <v-select
-                id="remuneracionFrecuencia"
-                v-model="ofertaDeTrabajo.remuneracion.frecuencia"
-                type="frecuencia"
-                :items="opciones"
-                item-text="nombre"
-                item-value="valor"
-                label="Tipo de Pago"
-                placeholder="Elige un método de pago"
-                prepend-icon="mdi-credit-card-outline"
-              ></v-select>
-            </v-col> -->
             <v-col cols="6">
               <v-text-field
                 id="monto"
@@ -152,18 +167,6 @@
               </v-text-field>
             </v-col>
             <v-col cols="3" class="mt-0 pt-0">
-              <!-- <v-select
-                id="divisa"
-                v-model="ofertaDeTrabajo.remuneracion.divisa"
-                type="divisa"
-                class = "mt-0"
-                :items="divisas"
-                item-text="nombre"
-                item-value="valor"
-                label="Divisa"
-                placeholder="Elige una Divisa"
-                prepend-icon="mdi-currency-usd"
-              ></v-select> -->
             </v-col>    
           </v-row>
             <v-card-subtitle class="indigo--text subtitle-1 mt-2 pb-1">
@@ -237,6 +240,101 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
+    <v-dialog v-model="dialogCalendario" max-width="1000">
+      <v-card>
+        <v-card-title class="text-h5">
+          Calendario de Trabajo
+        </v-card-title>
+        <v-card-text>
+          Aqui podrás modificar el calendario de trabajo.
+        </v-card-text>   
+          <v-row class="justify-center mx-2 pl-2">
+            <v-col cols=4>
+              <v-menu
+                id="menuFechaCalendario"
+                ref="menuFechaCalendario"
+                v-model="menuFechaCalendario"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template v-slot:activator="{ on, attrs }">
+                  <v-text-field
+                    id="fechaCalendario"
+                    v-model="fechaCalendario"
+                    label="Fecha Calendario"
+                    prepend-icon="mdi-calendar"
+                    type="fechaCalendario"
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="fechaCalendario"
+                  :active-picker.sync="fechaCalendarioPicker"
+                  :min="new Date().toISOString().substr(0, 10)"
+                  @change="guardarFechaCalendario"
+                  locale="es-VE"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+            <v-col cols=3>
+              <v-text-field v-model="horaInicio" label="Hora de inicio" placeholder="hh:mm:ss"></v-text-field>
+            </v-col>
+            <v-col cols=3>
+              <v-text-field v-model="horaFin" label="Hora de fin" placeholder="hh:mm:ss"></v-text-field>
+            </v-col>
+            <v-col cols=2>
+              <v-btn color="green" class="mt-4" text @click="agregarFechaCalendario">Agregar</v-btn>
+            </v-col>
+          </v-row>
+          <!-- <v-row class="mx-2" v-for="(fecha, index) in fechasCalendario" :key="index">
+            <div v-if="fecha.horaInicio.split(' ')[0]!==''">
+              <v-row>
+                <p class="ml-5 mt-2">Dia: {{fecha.horaInicio.split(" ")[0]}}
+                  Hora de inicio: {{fecha.horaInicio.split(" ")[1]}}
+                  Hora de fin: {{fecha.horaFin.split(" ")[1]}}
+                </p>
+                <v-btn color="red" text>Eliminar</v-btn>
+              </v-row>   
+            </div>
+          </v-row> -->
+           <v-data-table v-if="ofertaDeTrabajo.fechasCalendario.length!==0"
+                :headers="columnas_tabla"
+                :items="ofertaDeTrabajo.fechasCalendario"
+                class="elevation-1 mx-5"
+              >
+                <template v-slot:[`item.dia`]="{ item }">
+                  <span v-text="item.horaInicio.split(' ')[0]"></span>
+                </template>
+                <template v-slot:[`item.horaInicio`]="{ item }">
+                  <span v-text="item.horaInicio.split(' ')[1]"></span>
+                </template>
+                <template v-slot:[`item.horaFin`]="{ item }">
+                  <span v-text="item.horaFin.split(' ')[1]"></span>
+                </template>
+                <template v-slot:[`item.acciones`]="{ item }">
+                  <v-icon
+                    small
+                    @click="eliminarFechaCalendario(item)"
+                    color="red"
+                  >
+                    mdi-delete
+                  </v-icon>
+                </template>
+              </v-data-table>  
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn class="mr-2" color="indigo darken-4" text @click="dialogCalendario = false">
+            Cancelar
+          </v-btn>
+          <v-btn class="indigo darken-4" dark @click="dialogCalendario = false">
+            Aceptar
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -250,41 +348,107 @@
     name: 'FormularioOferta',
 
     data: () => ({
-      // opciones: [
-      //       {
-      //           nombre: "Por Hora",
-      //           valor: "hora"
-      //       },
-      //       {
-      //           nombre: "Por Mes",
-      //           valor: "mes"
-      //       },
-      //       {
-      //           nombre: "Por semana",
-      //           valor: "semana" 
-      //       },
-      //       {
-      //           nombre: "Total",
-      //           valor: "total"
-      //       }
-      //   ],
-      // divisas: [
-      //       {
-      //           nombre: "$ (Dólar)",
-      //           valor: "dolar"
-      //       },
-      //       {
-      //           nombre: "€ (Euro)",
-      //           valor: "euro"
-      //       },
-      //       {
-      //           nombre: "Bs. (Bolívar)",
-      //           valor: "bolivar"
-      //       }
-      //   ],
+      columnas_tabla: [
+        {
+          text: 'Día',
+          align: 'center',
+          sortable: true,
+          value: 'dia',
+          class: 'primary--text font-weight-bold',
+        },
+        {
+          text: 'Hora de Inicio',
+          align: 'center',
+          sortable: true,
+          value: 'horaInicio',
+          class: 'primary--text font-weight-bold',
+        },
+        {
+          text: 'Hora de Fin',
+          align: 'center',
+          sortable: false,
+          value: 'horaFin',
+          class: 'primary--text font-weight-bold'
+        },
+        {
+          text: 'Eliminar',
+          align: 'center',
+          sortable: false,
+          value: 'acciones',
+          class: 'primary--text font-weight-bold',
+        },
+      ],
+      horaInicio: "",
+      horaFin: "",
+      dialogCalendario: false,
+      habilidadesSeleccionadas: [],
+      certificacionesSeleccionadas: [],
+      habilidades: [
+        {
+          nombre: 'Habilidades Blandas',
+          valor: 1
+        },
+        {
+          nombre: 'Habilidades Técnicas',
+          valor: 2
+        },
+        {
+          nombre: 'Habilidades Cognitivas',
+          valor: 3
+        },
+        {
+          nombre: 'Habilidades Mecánicas',
+          valor: 4
+        },
+        {
+          nombre: 'Habilidades Linguísticas',
+          valor: 5
+        },
+        {
+          nombre: 'Habilidades Manuales',
+          valor: 6
+        },
+        {
+          nombre: 'Habilidades Visuales',
+          valor: 7
+        }
+      ],
+      certificaciones: [
+        {
+          nombre: 'Certificacion 1',
+          valor: "ab"
+        },
+        {
+          nombre: 'Certificacion 2',
+          valor: "bb"
+        },
+        {
+          nombre: 'Certificacion 3',
+          valor: "cb"
+        },
+        {
+          nombre: 'Certificacion 4',
+          valor: "db"
+        },
+        {
+          nombre: 'Certificacion 5',
+          valor: "eb"
+        },
+        {
+          nombre: 'Certificacion 6',
+          valor: "fb"
+        },
+        {
+          nombre: 'Certificacion 7',
+          valor: "gb"
+        }
+      ],
       fechaLimite: "",
       menuFechaLimite: false,
       fechaLimitePicker: "",
+      fechaCalendario: "",
+      menuFechaCalendario: false,
+      fechaCalendarioPicker: "",
       crearOfertaDialog: false,
       ofertaDeTrabajo: {
         titulo: "",
@@ -300,7 +464,8 @@
         },
         remuneracionPorHora: "",
         vacantes: "",
-        fechaLimite: ""
+        fechaLimite: "",
+        fechasCalendario:[] as {horaInicio: string, horaFin: string}[]
       },
       ofertaCreada: {
           mostrar: false,
@@ -312,17 +477,28 @@
       menuFechaLimite (val) {
         val && setTimeout(() => (this.fechaLimitePicker = 'YEAR'))
       },
+      menuFechaCalendario (val) {
+        val && setTimeout(() => (this.fechaCalendarioPicker = 'YEAR'))
+      },
     },
     methods: {
       guardarFechaLimite (fechaLimite: Date): void {
         (this.$refs.menuFechaLimite! as Vue & { save: (fechaLimite:any) => string }).save(fechaLimite)
       },
+      guardarFechaCalendario (fechaCalendario: Date): void {
+        (this.$refs.menuFechaCalendario! as Vue & { save: (fechaCalendario:any) => string }).save(fechaCalendario)
+      },
       cambiarEstadoCrearOfertaDialog(): void{
         this.crearOfertaDialog = !this.crearOfertaDialog
         this.limpiarFormulario()
       },
+      eliminarFechaCalendario(fecha: {horaInicio: string, horaFin: string}): void {
+        const index = this.ofertaDeTrabajo.fechasCalendario.findIndex((f: any) => f == fecha)
+        this.ofertaDeTrabajo.fechasCalendario.splice(index,1)
+      },
       limpiarFormulario(): void{
         this.ofertaDeTrabajo = {
+            fechasCalendario:[],
             duracion: "",
             titulo: "",
             cargo: "",
@@ -337,10 +513,21 @@
             remuneracionPorHora: "",
             vacantes: "",
             fechaLimite: ""
+
         }
       },
+      agregarFechaCalendario(): void{
+        this.ofertaDeTrabajo.fechasCalendario.push({
+          horaInicio: `${this.fechaCalendario} ${this.horaInicio}`,
+          horaFin: `${this.fechaCalendario} ${this.horaFin}`,
+        })
+
+        this.horaInicio = ""
+        this.horaFin = ""
+        this.fechaCalendario = ""
+      },
       crearOferta(){
-        
+        alert(this.ofertaDeTrabajo.fechasCalendario)
         const puertoOferta: UIPuertoCrearOferta = new CrearOfertaDeTrabajo()
         const oferta = {
             requerimientosEspeciales: this.ofertaDeTrabajo.requerimientosEspeciales,
@@ -350,7 +537,10 @@
             tituloTrabajo: {titulo : this.ofertaDeTrabajo.titulo},
             remuneracionPorHora: this.ofertaDeTrabajo.remuneracionPorHora,
             estadoOfertaDeTrabajo: 0,
-            vacantes: this.ofertaDeTrabajo.vacantes
+            vacantes: this.ofertaDeTrabajo.vacantes,
+            certificaciones: this.certificacionesSeleccionadas,
+            habilidades: this.habilidadesSeleccionadas,
+            calendario: this.ofertaDeTrabajo.fechasCalendario
             // empleador: {
             //     id: "5",
             // },
