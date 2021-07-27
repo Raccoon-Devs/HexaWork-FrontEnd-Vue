d77d8de2a9fae1c535252a5ec7fc90df9ca09a47
davidjzacarias <davidjzacarias@hotmail.com>
2021-07-26 22:48:20 -0400

    Fix: Algunos errores en Entidad OfertaDeTrabajo

--- src/core/dominio/OfertasDeTrabajo/OfertaDeTrabajo.ts
+++ src/core/dominio/OfertasDeTrabajo/OfertaDeTrabajo.ts
@@ -1,38 +1,15 @@
 import { Entidad } from "../ClasesBase/Entidad"
 import { IDOferta } from "./ValueObjects/IDOferta"
-import { TiempoLimitePostulacion, TiempoLimitePostulacionPropiedades } from "./ValueObjects/TiempoLimitePostulacion"
+import { TiempoLimitePostulacion } from "./ValueObjects/TiempoLimitePostulacion"
 import { Duracion } from "./ValueObjects/Duracion"
-import { Remuneracion, RemuneracionPropiedades } from "./ValueObjects/Remuneracion"
+import { Remuneracion } from "./ValueObjects/Remuneracion"
 import { EstadoOfertaDeTrabajo } from "./ValueObjects/EstadoOfertaDeTrabajo"
 import { Vacante } from "./ValueObjects/Vacante"
-import { Cargo } from "./ValueObjects/Cargo"
-//import { Descripcion } from "./ValueObjects/Descripcion"
-import { FechaPublicacion } from "./ValueObjects/FechaPublicacion"
-import { Empleador, EmpleadorPropiedades } from "../Empleador/Empleador"
-import { EmpleadorParaDominio } from "../Empleador/DTOEmpleador"
-//import { DireccionPropiedades } from "../Empleador/valueObjects/Direccion"
 import { TituloTrabajo } from "../valueObjectsComunes/TituloTrabajo"
 import { Calendario } from "../valueObjectsComunes/Calendario"
 import { Direccion } from "../valueObjectsComunes/Direccion"
-import { Habilidad } from "../habilidad/Habilidad"
-import { Certificacion } from "../certificacion/Certificacion"
 import { RequerimientosEspecialesOfertaDeTrabajo } from "./ValueObjects/RequerimientosEspecialesOfertaDeTrabajo"
 
-
-// export interface OfertaDeTrabajoPropiedades {
-//     id: IDOferta
-//     titulo: TituloOferta
-//     empleador: Empleador
-//     direccion: Direccion
-//     fechaLimite: TiempoLimitePostulacion
-//     duracion: Duracion
-//     remuneracion: Remuneracion
-//     estadoOfertaDeTrabajo:  EstadoOfertaDeTrabajo
-//     vacante: Vacante
-//     cargo: Cargo
-//     descripcion: Descripcion
-//     fechaPublicacion: FechaPublicacion | null
-// }
 export interface OfertaDeTrabajoPropiedades{
     idOfertaDeTrabajo: IDOferta,
     tituloTrabajo: TituloTrabajo,
@@ -49,21 +26,6 @@ export interface OfertaDeTrabajoPropiedades{
     vacantes: Vacante
 }
 
-// type direccionOferta = {
-//     calle1: string,
-//     calle2: string,
-//     ciudad: string,
-//     estado: string,
-//     zip: string
-// }
-
-// type remuneracionOferta = {
-//     monto: number,
-//     frecuencia: string,
-//     divisa: string
-// }
-
-
 export class OfertaDeTrabajo extends Entidad<OfertaDeTrabajoPropiedades> {
 
     private constructor (propiedades: OfertaDeTrabajoPropiedades) {
@@ -73,10 +35,6 @@ export class OfertaDeTrabajo extends Entidad<OfertaDeTrabajoPropiedades> {
     obtenerTitulo(): string{
         return this.propiedades.tituloTrabajo.obtenerTitulo()
     }
-
-    // obtenerDireccion(): DireccionPropiedades{
-    //     return this.propiedades.direccion.obtenerDireccion()
-    // }
 
     obtenerFechaLimite(): Date{
         return this.propiedades.fechaLimitePostulacionOfertaDeTrabajo.obtenerFechaLimite()
@@ -93,14 +51,6 @@ export class OfertaDeTrabajo extends Entidad<OfertaDeTrabajoPropiedades> {
     obtenerVacante(): number{
         return this.propiedades.vacantes.obtenerVacante()
     }
-
-    // obtenerCargo(): Cargo{
-    //     return this.propiedades.cargo
-    // }
-
-    // obtenerFechaPublicacion(): FechaPublicacion | null{
-    //     return this.propiedades.fechaPublicacion
-    // }
 
     obtenerId(): string | number{
         return this.propiedades.idOfertaDeTrabajo.obtenerId()
@@ -138,6 +88,10 @@ export class OfertaDeTrabajo extends Entidad<OfertaDeTrabajoPropiedades> {
     //     return this.propiedades.empleador
     // }
 
+    obtenerOfertaDeTrabajo(): OfertaDeTrabajoPropiedades{
+        return this.propiedades
+    }
+
     public static update(ofertaDeTrabajo: any,): OfertaDeTrabajo {
 
         return new OfertaDeTrabajo(
@@ -153,10 +107,7 @@ export class OfertaDeTrabajo extends Entidad<OfertaDeTrabajoPropiedades> {
                 //empleador: propiedades.empleador,
                 direccion:propiedades.direccion,
                 fechaLimitePostulacionOfertaDeTrabajo: propiedades.fechaLimitePostulacionOfertaDeTrabajo,
-                //calendario: propiedades.calendario,
-                //habilidades: propiedades.habilidades,
                 requerimientosEspeciales: propiedades.requerimientosEspeciales,
-                //certificaciones: propiedades.certificaciones,
                 duracion: propiedades.duracion,
                 remuneracionPorHora: propiedades.remuneracionPorHora,
                 estadoOfertaDeTrabajo: propiedades.estadoOfertaDeTrabajo,
