976292555c24fcbc1cb855c9262a73a691658aa8
Sofia Rodriguez <sofiaromorales@gmail.com>
2021-07-31 16:27:08 -0400

    WIP: Tabla trabajos culminados

---
+++ src/infraestructura/components/TrabajosFinalizadosCard.vue
@@ -0,0 +1,102 @@
+<template>
+    <v-container>
+        <v-expansion-panels focusable>
+            <v-expansion-panel
+              v-for="(oferta, index) in ofertas"
+              :key="index"
+            >
+                <v-expansion-panel-header>
+                    <v-container>
+                        <v-row
+                            no-gutters
+                        >
+                            <v-col cols="auto">
+                                <h3 class="font-weight-black mr-8 indigo--text">
+                                    {{oferta.tituloTrabajo}}
+                                </h3>
+                            </v-col>
+                            <v-col class="mt-1">
+                                <h4>
+                                    {{oferta.duracion.totalHorasRequeridas}}h. requeridas
+                                </h4>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                    <template
+                        v-for="(empleado, indexE) in oferta.empleados"
+                    >
+                        <v-container
+                            :key="indexE"
+                        >
+                            <v-row
+                                no-gutters
+                            >
+                                <v-col>
+                                    {{empleado.nombreCompleto}}
+                                </v-col>
+                                <v-spacer></v-spacer>
+                                <v-col cols="auto">
+                                    
+                                </v-col>
+                            </v-row>
+                        </v-container >
+                        <v-divider
+                            :key="indexE"
+                        >
+                        </v-divider>
+                    </template>
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </v-container>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        name: 'TrabajosFinalizadosCard',
+        data: () => ({
+            ofertas: [
+                {
+                    idOfertaDeTrabajo: 1,
+                    tituloTrabajo: 'Titulo de trabajo',
+                    duracion: {
+                        totalHorasRequeridas: 20
+                    },
+                    //empleador: Empleador,
+                    empleados: [{
+                        ssn: '123',
+                        nombreCompleto: 'Nombre empleado'
+                    }, {
+                        ssn: '456',
+                        nombreCompleto: 'Nombre empleado'
+                    }, {
+                        ssn: '789',
+                        nombreCompleto: 'Nombre empleado'
+                    }]
+                },
+                {
+                    idOfertaDeTrabajo: 1,
+                    tituloTrabajo: 'Titulo de trabajo',
+                    duracion: {
+                        totalHorasRequeridas: 20
+                    },
+                    //empleador: Empleador,
+                    empleados: [{
+                        ssn: '123',
+                        nombreCompleto: 'Nombre empleado'
+                    }, {
+                        ssn: '456',
+                        nombreCompleto: 'Nombre empleado'
+                    }, {
+                        ssn: '789',
+                        nombreCompleto: 'Nombre empleado'
+                    }]
+                },
+            ]
+        })
+    })
+</script>
--- src/infraestructura/views/OfertasTerminadas.vue
+++ src/infraestructura/views/OfertasTerminadas.vue
@@ -1,20 +1,26 @@
 <template>
      <v-container>
-         <h2 class="py-4 grey--text">
-            Ofertas culminadas
-         </h2>
-         <RelacionesPorEvaluarCard/>
+        <h2 class="py-4 grey--text">
+            Ofertas Culminadas por Evaluar
+        </h2>
+        <RelacionesPorEvaluarCard/>
+        <h2 class="py-4 grey--text">
+            Trabajos Culminados
+        </h2>
+        <TrabajosFinalizadosCard/>
      </v-container>
  </template>
 
  <script lang="ts">
     import Vue from 'vue'
     import RelacionesPorEvaluarCard from '../components/RelacionesPorEvaluarCard'
+    import TrabajosFinalizadosCard from '../components/TrabajosFinalizadosCard'
 
     export default Vue.extend({
         name: 'OfertasTerminadas',
         components: {
-            RelacionesPorEvaluarCard
+            RelacionesPorEvaluarCard,
+            TrabajosFinalizadosCard
         }
     })
  </script>
