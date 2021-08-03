ce9166158ac1f1e97ef360c53c0630608b02a790
davidjzacarias <davidjzacarias@hotmail.com>
2021-08-03 12:11:43 -0400

    fix: Error en adaptador

--- src/core/infraestructura/adaptadores/adaptadoresMock/adaptadorMockRelacionDeTrabajo.ts
+++ src/core/infraestructura/adaptadores/adaptadoresMock/adaptadorMockRelacionDeTrabajo.ts
@@ -1,10 +1,10 @@
 import { APIPuertoRelacionesDeTrabajo } from "../../../aplicacion/api/relacionesDeTrabajo/APIPuertoRelacionesDeTrabajo";
 import { obtenerRelacionesPorEvaluar } from "../../persistencia/relacionDeTrabajoPorEvaluar";
-// import { obtenerRelacionesDeTrabajoMock } from "./../../persistencia/relacionesDeTrabajoMock";
+import { obtenerRelacionesDeTrabajoMock } from "./../../persistencia/relacionesDeTrabajoMock";
 
 export class AdaptadorMockRelacionDeTrabajo extends APIPuertoRelacionesDeTrabajo {
     public listarRelacionesDeTrabajoMock(): any {
-        // return obtenerRelacionesDeTrabajoMock()
+        return obtenerRelacionesDeTrabajoMock()
     }
 
     public listarRelacionesPorEvaluar(): any {
