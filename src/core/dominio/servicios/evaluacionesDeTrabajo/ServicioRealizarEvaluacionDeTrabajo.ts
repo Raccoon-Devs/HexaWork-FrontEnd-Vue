b774d90e31462ef1f01babe1456daafff2977cb3
davidjzacarias <davidjzacarias@hotmail.com>
2021-08-04 14:35:55 -0400

    Fix: Algunos cambios en el servicio de aplicación

--- src/core/dominio/servicios/evaluacionesDeTrabajo/ServicioRealizarEvaluacionDeTrabajo.ts
+++ src/core/dominio/servicios/evaluacionesDeTrabajo/ServicioRealizarEvaluacionDeTrabajo.ts
@@ -1,22 +1,10 @@
+import { EvaluacionDeTrabajoParaDominio } from "../../evaluacionDeTrabajo/DTOEvaluacionDeTrabajo";
 import { EvaluacionDeTrabajo } from "../../evaluacionDeTrabajo/EvaluacionDeTrabajo";
 
 export class ServicioRealizarEvaluacionDeTrabajo{
     
-    static pasarADominio(evaluacionAtributos: any): EvaluacionDeTrabajo{
+    static pasarADominio(evaluacionAtributos: EvaluacionDeTrabajoParaDominio): EvaluacionDeTrabajo{
 
-        return EvaluacionDeTrabajo.crear(
-            {
-                idEvaluacion: evaluacionAtributos.idEvaluacion,
-                preguntas: evaluacionAtributos.preguntas,
-                puntuaciones: evaluacionAtributos.puntuaciones,
-                puntuacionPromedio: evaluacionAtributos.puntuacionPromedio,
-                trabajo: {
-                    idRelacion: evaluacionAtributos.trabajo.idRelacion,
-                    postulacion: evaluacionAtributos.trabajo.postulacion,
-                    calendario: evaluacionAtributos.trabajo.calendario,
-                    estadoRelacionDeTrabajo: evaluacionAtributos.trabajo.estadoRelacionDeTrabajo
-                }
-            }
-        )
+        return EvaluacionDeTrabajo.crear(evaluacionAtributos)
     }
 }
--- src/core/infraestructura/tests/integracion/realizarEvaluacionLocal.test.ts
+++ src/core/infraestructura/tests/integracion/realizarEvaluacionLocal.test.ts
@@ -1,103 +1,195 @@
 import { AdaptadorTIEvaluacion } from "../../adaptadores/adaptadoresIntegracion/adaptadorTIEvaluacion";
 import { UIPuertoEvaluacionDeTrabajo } from "../../../aplicacion/ui/evaluacionesDeTrabajo/UIPuertoEvaluacionDeTrabajo";
 import { RealizarEvaluacion } from "../../../aplicacion/servicios/evaluacionesDeTrabajo/RealizarEvaluacion";
-import { EvaluacionDeTrabajoParaDominio } from "../../../dominio/evaluacionDeTrabajo/DTOEvaluacionDeTrabajo";
-import { IDRelacionDeTrabajo } from "../../../dominio/relacionDeTrabajo/valueObjects/IDRelacionDeTrabajo";
-import { IDPostulacion } from "../../../dominio/postulacion/valueObjects/IDPostulacion";
-import { SSN } from "../../../dominio/empleado/valueObjects/SSN";
-import { EstadoRelacionDeTrabajo } from "../../../dominio/relacionDeTrabajo/valueObjects/EstadoRelacionDeTrabajo";
-import { Calendario } from "../../../dominio/valueObjectsComunes/Calendario";
-import { FechaPostulacion } from "../../../dominio/postulacion/valueObjects/Fechapostulacion";
-import { Postulacion } from "../../../dominio/postulacion/Postulacion";
-import { NombreCompleto } from "../../../dominio/valueObjectsComunes/NombreCompleto";
-import { Telefono } from "../../../dominio/valueObjectsComunes/Telefono";
-import { FechaNacimiento } from "../../../dominio/empleado/valueObjects/FechaNacimiento";
-import { NivelEducativo } from "../../../dominio/empleado/valueObjects/NivelEducativo";
-import { Clasificacion } from "../../../dominio/empleado/valueObjects/Clasificacion";
-import { EstadoEmpleado } from "../../../dominio/empleado/valueObjects/EstadoEmpleado";
-import { Direccion } from "../../../dominio/valueObjectsComunes/Direccion";
-import { IDOferta } from "../../../dominio/ofertasDeTrabajo/valueObjects/IDOferta";
-import { TituloTrabajo } from "../../../dominio/valueObjectsComunes/TituloTrabajo";
-import { TiempoLimitePostulacion } from "../../../dominio/ofertasDeTrabajo/valueObjects/TiempoLimitePostulacion";
-import { IDHabilidad } from "../../../dominio/habilidad/valueObjects/IDHabilidad";
-import { CategoriaHabilidad } from "../../../dominio/habilidad/valueObjects/CategoriaHabilidad";
-import { NombreHabilidad } from "../../../dominio/habilidad/valueObjects/NombreHabilidad";
-import { Habilidad } from "../../../dominio/habilidad/Habilidad";
-import { RequerimientosEspecialesOfertaDeTrabajo } from "../../../dominio/ofertasDeTrabajo/valueObjects/RequerimientosEspecialesOfertaDeTrabajo";
-import { Duracion } from "../../../dominio/ofertasDeTrabajo/valueObjects/Duracion";
-import { Remuneracion } from "../../../dominio/ofertasDeTrabajo/valueObjects/Remuneracion";
-import { EstadoOfertaDeTrabajo } from "../../../dominio/ofertasDeTrabajo/valueObjects/EstadoOfertaDeTrabajo";
-import { Vacante } from "../../../dominio/ofertasDeTrabajo/valueObjects/Vacante";
-import { Certificacion } from "../../../dominio/certificacion/Certificacion";
-import { OfertaDeTrabajo } from "../../../dominio/ofertasDeTrabajo/OfertaDeTrabajo";
-import { Empleado } from "../../../dominio/empleado/Empleado";
-
 
 //Realizar evaluación de trabajo
 test('Realizar evaluación de trabajo', () => {
 
     const puertoEvaluacion: UIPuertoEvaluacionDeTrabajo = new RealizarEvaluacion()
-    const oferta = 
+    const experiencias: any = [
         {
-            idOfertaDeTrabajo: 1,
-            tituloTrabajo: "Titulo",
-            //empleador: Empleador,
-            direccion: {
-                calle1: "calle1",
-                calle2: "calle2",
-                ciudad: "ciudad",
-                estado: "estado",
-                codPostal: "1010"
-            }   ,
-            fechaLimitePostulacionOfertaDeTrabajo: new Date("2020-07-05"),
-            calendario: [
-                {
-                    horaInicio: "2020-08-08 05:05:55",
-                    horaFin: "2020-08-08 06:05:55"
-                }
-            ],
+            idExperiencia: '1',
+            tituloTrabajo: 'Titulo Trabajo 1',
+            nombreCompania: 'Compañia 1',
+            categoria: 0
+        },
+        {
+            idExperiencia: '2',
+            tituloTrabajo: 'Titulo Trabajo 2',
+            nombreCompania: 'Compañia 2',
+            categoria: 1
+        },
+    ]
+    
+    const habilidades: any = [
+        {
+            idHabilidad: '1',
+            nombre: 'Habilidades blandas',
+            categoria: 0
+        },
+        {
+            idHabilidad: '2',
+            nombre: 'Habilidades técnicas',
+            categoria: 1
+        },
+    ]
+    
+    const cursos: any = [
+        {
+            idCurso: '1',
+            codigo: 'Curso 1',
+            titulo: 'Título Curso 1',
+            imagenPortada: 'Imagen 1',
             habilidades: [
-                {
-                    idHabilidad: 2,
-                    nombre: "Habilidad 1",
-                    categoria: 2
-                }
+                habilidades[0],
+                habilidades[1]
             ],
-            requerimientosEspeciales: "Requerimientos especiales",
-            certificaciones: [
-                {
-                    idCertificacion: "3",
-                    nombre: "Certificacion 3",
-                    fechaCompletacionCertificacion: "2021-03-03",
-                    curso: {
-                        idCurso: "3",
-                        codigo: "Curso 3",
-                        titulo: "Titulo curso 3",
-                        imagenPortada: "Imagen",
-                        estadoCurso: 0,
-                        habilidades: [{
-                            idHabilidad: '1',
-                            nombre: 'Habilidades blandas',
-                            categoria: 0
-                        }],
-                    }
-                }
+            estadoCurso: 0
+        },
+        {
+            idCurso: '2',
+            codigo: 'Curso 2',
+            titulo: 'Título Curso 2',
+            imagenPortada: 'Imagen 2',
+            habilidades: [
+                habilidades[0],
+                habilidades[1]
             ],
-            duracion: 5,
-            remuneracionPorHora: 3.44,
-            estadoOfertaDeTrabajo: 1,
-            vacantes: 5
+            estadoCurso: 0
+        },
+    ]
+    
+    const referencias: any = [
+        {
+            idReferencia: '1',
+            nombreCompleto: {
+                primerNombre: 'Primer Nombre Ref 1',
+                segundoNombre: 'Segundo Nombre Ref 1',
+                primerApellido: 'Primer Apellido Ref 1',
+                segundoApellido: 'Segundo Apellido Ref 1'
+            },
+            tituloTrabajo: 'Titulo Trabajo 1',
+            nombreCompania: 'Compañia 1',
+            numeroTelefono: {
+                codigoPais: 111,
+                numeroTelefono: 1111111111
+            },
+            correo: 'correo1@example.com'
+        },
+        {
+            idReferencia: '2',
+            nombreCompleto: {
+                primerNombre: 'Primer Nombre Ref 2',
+                segundoNombre: 'Segundo Nombre Ref 2',
+                primerApellido: 'Primer Apellido Ref 2',
+                segundoApellido: 'Segundo Apellido Ref 2'
+            },
+            tituloTrabajo: 'Titulo Trabajo 2',
+            nombreCompania: 'Compañia 2',
+            numeroTelefono: {
+                codigoPais: 222,
+                numeroTelefono: 2222222222
+            },
+            correo: 'correo2@example.com'
+        },
+    ]
+    
+    const empleado: any = {
+        ssn: '1111',
+        nombreCompleto: {
+            primerNombre: 'Nombre1',
+            segundoNombre: 'Segundo Nombre 1',
+            primerApellido: 'Apellido1',
+            segundoApellido: 'Segundo Apellido 1'
+        },
+        numeroTelefono: {
+            codigoPais: 333,
+            numeroTelefono: 3333333333
+        },
+        fechaNacimiento: '2000-01-01',
+        nivelEducativo: 10,
+        experienciaLaboral: experiencias,
+        habilidades: habilidades,
+        cursos: cursos,
+        referencias: referencias,
+        clasificacion: 4,
+        estadoEmpleado: 2,
+        direccion: {
+            calle1: 'calle1',
+            calle2: 'calle2',
+            ciudad: 'ciudad',
+            estado: 'estado',
+            codPostal: 'codPostal'
         }
+    }
     
+    const ofertaDeTrabajo = {
+        idOfertaDeTrabajo: "IdPorDefecto",
+        tituloTrabajo: "Título oferta",
+        //empleador: Empleador,
+        direccion: {
+            calle1: "calle1",
+            calle2: "calle2",
+            ciudad: "ciudad",
+            estado: "estado",
+            codPostal: "codigoPos"
+        },
+        fechaLimitePostulacionOfertaDeTrabajo: "2020-07-18",
+        calendario: [{horaInicio: "2020-08-08 05:05:55", horaFin: "2020-08-08 06:05:55"}],
+        habilidades: [
+            {
+                idHabilidad: "1",
+                categoria: 1,
+                nombre: "HabilidadesBlandas"
+            }
+        ],
+        requerimientosEspeciales: "RequerimientosEspeciales",
+        certificaciones: [
+            {
+                idCertificacion: "3",
+                nombre: "Certificacion 3",
+                fechaCompletacionCertificacion: "2021-03-20",
+                curso: {
+                    idCurso: "3",
+                    codigo: "Curso 3",
+                    titulo: "Titulo curso 3",
+                    imagenPortada: "Imagen",
+                    estadoCurso: 0,
+                    habilidades: [{
+                        idHabilidad: '1',
+                        nombre: 'Habilidades blandas',
+                        categoria: 0
+                    }],
+                }
+            }
+        ],
+        duracion: 5,
+        remuneracionPorHora: 3.44,
+        estadoOfertaDeTrabajo: 4,
+        vacantes: 5
+    }
     
-    const evaluacion:EvaluacionDeTrabajoParaDominio  = {
-        idEvaluacion: "",
+    const relacionesDeTrabajo = {
+        idRelacion: "",
+        estadoRelacionDeTrabajo: 1,
+        calendario: [],
+        postulacion: {
+            idPostulacion: '1',
+            fechaPostulacion: '2020-07-02',
+            empleado: empleado,
+            ofertaDeTrabajo: ofertaDeTrabajo,
+            estadoPostulacion: 0,
+        }
+    }
+    
+    const evaluacion = {
+        idEvaluacion: '1',
         preguntas: {
-            pregunta1: "pregunta1",
-            pregunta2: "pregunta2",
-            pregunta3: "pregunta3",
-            pregunta4: "pregunta4",
-            pregunta5: "pregunta5"
+            pregunta1: 'pregunta1',
+            pregunta2: 'pregunta2',
+            pregunta3: 'pregunta3',
+            pregunta4: 'pregunta4',
+            pregunta5: 'pregunta5'
         },
         puntuaciones: {
             puntuacion1: 4,
@@ -107,56 +199,8 @@ test('Realizar evaluación de trabajo', () => {
             puntuacion5: 4
         },
         puntuacionPromedio: 4,
-        trabajo: {
-            idRelacion: IDRelacionDeTrabajo.crear("1"),
-            postulacion: Postulacion.crear(
-                {
-                    idPostulacion: 1,
-                    fechaPostulacion: {
-                        fecha: new Date("2020-05-05"),
-                        limite: new Date("2020-06-06")
-                    },
-                    empleado: Empleado.crear({
-                        ssn: "ssnempleado",
-                        nombreCompleto: {
-                            primerNombre: "Primer Nombre",
-                            segundoNombre: "Segundo Nombre",
-                            primerApellido: "Primer Apellido",
-                            segundoApellido:"Segundo Apellido"
-                        },
-                        numeroTelefono: {
-                            codigoPais: 58,
-                            numeroTelefono: 4145214151
-                        },
-                        fechaNacimiento: new Date("1995-05-05"),
-                        nivelEducativo: 0,
-                        experienciaLaboral: [],
-                        habilidades: [
-                            {
-                                idHabilidad: 2,
-                                nombre: "Habilidad 1",
-                                categoria: 2
-                            }
-                        ],
-                        cursos: [],
-                        referencias: [],
-                        clasificacion: 5,
-                        estadoEmpleado: 2,
-                        direccion: {
-                            calle1: "calle1",
-                            calle2: "calle2",
-                            ciudad: "ciudad",
-                            estado: "estado",
-                            codPostal: "1010"
-                        }    
-                    }),
-                    ofertaDeTrabajo: oferta,
-                    estadoPostulacion: 1
-                }
-            ),
-            calendario: [Calendario.crear({horaInicio: "2020-08-08 05:05:55", horaFin: "2020-08-08 06:05:55"})],
-            estadoRelacionDeTrabajo: EstadoRelacionDeTrabajo.crear(1)
-        },
+        trabajo: relacionesDeTrabajo,
     }
+    
     expect(puertoEvaluacion.realizarEvaluacionUI(evaluacion, new AdaptadorTIEvaluacion())).toEqual("Evaluación realizada con éxito");
 });
